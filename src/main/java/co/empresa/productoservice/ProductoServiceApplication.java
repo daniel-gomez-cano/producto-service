dd8827fd1bcc5aedc6ad2f08e3214d4b57e0478f
daniel-gomez-cano <daniel.gomez.cano@correounivalle.edu.co>
2026-03-21 20:20:43 -0500

    chore: Proyecto base v0.1.0

--- src/main/java/co/empresa/productoservice/ProductoServiceApplication.java
+++ src/main/java/co/empresa/productoservice/ProductoServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductoServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProductoServiceApplication.class, args);
     }
-
 }
