8176725867543af5ac858465d027a2bc91ea0cef
daniel-gomez-cano <daniel.gomez.cano@correounivalle.edu.co>
2026-03-22 22:49:26 -0500

    chore: proyecto base v0.15.0

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 1', 'Descripcion 1', 100);
-INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 2', 'Descripcion 2', 200);
-INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 3', 'Descripcion 3', 300);
-INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 4', 'Descripcion 4', 400);
-INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 5', 'Descripcion 5', 500);
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 1', 'Descripcion 1', 100,2,'hola');
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 2', 'Descripcion 2', 200,3,'por favor');
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 3', 'Descripcion 3', 300,4,'que');
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 4', 'Descripcion 4', 400,5,'funcione');
+INSERT INTO producto (nombre, descripcion, precio) VALUES ('Producto 5', 'Descripcion 5', 500,6,'pls');
